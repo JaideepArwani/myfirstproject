bdc77f88c2647b30ce46d4a2af174cb5fc7ea72d
JaideepArwani <77190655+JaideepArwani@users.noreply.github.com>
2021-01-09 02:31:36 -0800

    uploaded my first java project

---
+++ src/myfirstproject/MyFirstProject.java
@@ -0,0 +1,16 @@
+package myfirstproject;
+
+
+public class MyFirstProject {
+
+
+    public static void main(String[] args) {
+        
+        System.out.println("Govinda");
+        System.out.println("Govinda");
+        System.out.println("Govinda");
+
+        
+    }
+    
+}
